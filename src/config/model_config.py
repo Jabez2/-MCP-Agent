0e243745717cc68d4ec22d14e3e8adcb879af38f
Jabez <jabez@192.168.1.7>
2025-08-06 20:40:33 +0800

    增加模型配置

--- src/config/model_config.py
+++ src/config/model_config.py
@@ -8,6 +8,24 @@ from autogen_core.models import ModelInfo
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 
+# def create_model_client():
+#     """创建LLM模型客户端"""
+#     model_info = ModelInfo(
+#         family="openai",
+#         vision=False,
+#         function_calling=True,
+#         json_output=True
+#     )
+#     return OpenAIChatCompletionClient(
+#         model="Qwen/Qwen3-Coder-480B-A35B-Instruct",
+#         api_key="ms-d00638ea-e181-40b9-9fba-8047d018acf0",
+#         base_url="https://api-inference.modelscope.cn/v1/",
+#         model_info=model_info,
+#         temperature=0.7,
+#         top_p=0.8,
+#         extra_body={"top_k": 20, "repetition_penalty": 1.05}
+#     )
+
 def create_model_client():
     """创建LLM模型客户端"""
     model_info = ModelInfo(
@@ -17,16 +35,14 @@ def create_model_client():
         json_output=True
     )
     return OpenAIChatCompletionClient(
-        model="Qwen/Qwen3-Coder-480B-A35B-Instruct",
-        api_key="ms-d00638ea-e181-40b9-9fba-8047d018acf0",
-        base_url="https://api-inference.modelscope.cn/v1/",
+        model="qwen3-coder-plus-2025-07-22",
+        api_key="sk-5a5419a94d8d4cbda5c302b80a69a47d",
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
         model_info=model_info,
         temperature=0.7,
         top_p=0.8,
         extra_body={"top_k": 20, "repetition_penalty": 1.05}
     )
-
-
 # def create_model_client():
 #     """创建LLM模型客户端
     
